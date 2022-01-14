a1d1ceb0cbb7848843e7eda75aa51ece95995b07
ArsenalNox <nohtimore@gmail.com>
2022-01-14 18:48:55 +0500

    расширение функционала
    - больше цветов
    - возможность рисовать
    - изменение размера кисти
    - очищение канваса

--- main.js
+++ main.js
@@ -1,46 +1,88 @@
 var current_color = null
 var canvas = document.getElementById('canvas1')
 var ctx = canvas.getContext('2d')
+
 var dimensions = {
     x: canvas.width,
     y: canvas.height
 }
 
-var isMouseClicked = false
+var brushSize = 2
+
+var mouse = {
+    posX: null,
+    posY: null,
+    isHeldDown: false
+}
+
+mouse.updatePos = function (e) {
+    var boundRect = canvas.getBoundingClientRect()
+    this.posX = e.clientX - boundRect.left
+    this.posY = e.clientY - boundRect.top
+}
+
 
 function load_main_colors(){
+    isFirstColor = true
     for(let color_cont of document.getElementsByClassName('color')){
         color_cont_color = color_cont.getAttribute('picker_color')
-        console.log(color_cont_color)
         color_cont.style.backgroundColor = color_cont_color
         color_cont.addEventListener('click', (e)=>{
+            for (let color_cont of document.getElementsByClassName('color')){
+                color_cont.classList.remove('active-color')
+            }
+            e.target.classList.add('active-color')
             current_color = e.target.getAttribute('picker_color')
-            console.log(current_color)
         })
+        if(isFirstColor){
+            current_color = color_cont_color
+            isFirstColor = false
+        }
     }
-    return 1
 }
 
 
 function load_custom_colors(){
+}
+
+
+function downloadPicture(){
 
 }
 
+
+function clearCanvas(){
+    ctx.clearRect(0, 0, dimensions.x, dimensions.y)
+}
+
+
+function addColor(){
+
+}
 
 
 load_custom_colors()
 load_main_colors()
 
-canvas.addEventListener('mousemove', () => {
-    if(isMouseClicked){
-        console.log('nowDrawing')
+canvas.addEventListener('mousemove', (e) => {
+    mouse.updatePos(e)
+
+    if(mouse.isHeldDown){
+        ctx.beginPath();
+        ctx.arc(mouse.posX, mouse.posY, 1+brushSize, 0, 2*Math.PI)
+        ctx.strokeStyle = current_color
+        ctx.stroke()
     }
 })
 
-canvas.addEventListener('mousedown', () => {
-    isMouseClicked = true
+document.addEventListener('mousedown', () => {
+    mouse.isHeldDown = true
 })
 
-canvas.addEventListener('mouseup', () => {
-    isMouseClicked = false
+document.addEventListener('mouseup', () => {
+    mouse.isHeldDown = false
+})
+
+document.getElementById('brushSizeSelector').addEventListener('change', (e) => {
+    brushSize = e.target.value
 })
